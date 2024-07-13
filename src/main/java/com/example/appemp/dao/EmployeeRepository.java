4684e426bfa138614937163eac537f0a451f9026
Trung Kiên <153173408+trungkienn67@users.noreply.github.com>
2024-07-13 10:55:29 +0700

    Add files via upload

---
+++ src/main/java/com/example/appemp/dao/EmployeeRepository.java
@@ -0,0 +1,16 @@
+package com.example.appemp.dao;
+
+import com.example.appemp.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // that's it ... no need to write any code LOL!
+
+    // add a method to sort by last name
+    public List<Employee> findAllByOrderByLastNameAsc();
+
+}
---
+++ src/main/java/com/example/appemp/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.example.appemp.service;
+
+import com.example.appemp.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    public List<Employee> findAll();
+
+    public Employee findById(int theId);
+
+    public void save(Employee theEmployee);
+
+    public void deleteById(int theId);
+
+}
